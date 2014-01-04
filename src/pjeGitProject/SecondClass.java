a0d315beac5ef733ea907b93c5694043668950c8
Geos <geos@geos-VirtualBox.(none)>
2014-01-04 20:40:32 +0100

    nex second class

---
+++ src/pjeGitProject/SecondClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pjeGitProject;
+
+/**
+ *
+ * @author geos
+ */
+public class SecondClass {
+    
+    public void ShowMessage(){
+        System.out.println("Message from Second Class ");
+    }
+}
